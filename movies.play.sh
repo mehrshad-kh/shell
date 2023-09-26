7e85f78cd48449894e0ce564a92a6cbafc719369
mehrshad-kh <mjii@protonmail.com>
2023-09-26 22:06:19 +0330

    fix(movies.play.sh): improve option handling

--- movies.play.sh
+++ movies.play.sh
@@ -3,22 +3,25 @@
 set -euo pipefail
 
 zmodload zsh/zutil
-zparseopts {s,-seek}:=seek_arg || exit 1
+zparseopts {s,-seek}:=seek_value || exit 1
 
-if [[ $# -eq 0 ]]; then
-    if [ -h latest ]; then
-        # -sn   disable subtitles
-        # -ss pos   seek pos. Check format man page.
-        ffplay $PWD/latest -fs -autoexit
-    else
+if [[ ! -h latest ]]; then
         echo "error: latest: no such file" >&2
         echo "Have you run \`movies link\'?" >&2
         exit 1
-    fi
+fi
+
+if [[ $# -eq 0 ]]; then
+    # -sn   disable subtitles
+    # -ss pos   seek pos. Check format man page.
+    ffplay latest -fs -autoexit
 else
-    if [[ $# -eq 2 ]]; then
-        ffplay $PWD/latest -fs -autoexit -ss ${seek_arg[2]}
+    if [[ ${#seek_value[@]} -eq 0 ]]; then
+        echo "usage: movies play [ -s | --seek ] seconds"
+        exit 1
     fi
+
+    ffplay latest -fs -autoexit -ss ${seek_value[2]}
 fi
 
 exit 0
