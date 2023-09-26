c4783bb78310d44c5416a73a4fd0fb5a806284f8
mehrshad-kh <mjii@protonmail.com>
2023-09-26 22:13:14 +0330

    refactor(movies.init.sh)

--- movies.init.sh
+++ movies.init.sh
@@ -5,21 +5,20 @@
 set -euo pipefail
 
 zmodload zsh/zutil
-zparseopts {n,-name}:=name_arg {u,-url}:=url_arg || exit 1
+zparseopts {n,-name}:=name_value {u,-url}:=url_value || exit 1
 
-if [ $# -eq 0 ] || [ ${#name_arg[@]} -eq 0 ] || [ ${#url_arg[@]} -eq 0 ]
-then
+if [[ $# -eq 0 ]] || [[ ${#name_value[@]} -eq 0 ]] || [[ ${#url_value[@]} -eq 0 ]]; then
     echo "usage: init -n name -u url"
     exit 1
 fi
 
-if [ -f $PWD/.info ]; then
+if [[ -f .info ]]; then
     echo "fatal: movies repository already exists in $PWD/" >&2
     exit 1
-else
-    echo "name=${name_arg[2]}" >> $PWD/.info
-    echo "url=${url_arg[2]}" >> $PWD/.info
-    echo "Initialized .info in $(pwd -P)/"
 fi
+
+echo "name=${name_value[2]}" >> .info
+echo "url=${url_value[2]}" >> .info
+echo "Initialized .info in $(pwd -P)/"
 
 exit 0
