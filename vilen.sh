72d7b1cbf1c04ab0c34e82c41707237730d1cc46
mehrshad-kh <mjii@protonmail.com>
2023-10-01 17:19:41 +0330

    feat: use vilen.sh for local files
    update movies.dir

--- vilen.sh
+++ vilen.sh
@@ -9,10 +9,15 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-for link in "$@"; do
-    resolved_link=$(curl -L --head -w '%{url_effective}' ${link} 2>/dev/null | tail -n1)
-    video_len=$(ffmpeg -i ${resolved_link} 2>&1 | grep "Duration" | cut -d " " -f 4 | sed s/,//)
-    # ffprobe -i $file -show_entries format=duration -sexagesimal -v quiet -of csv="p=0"
+for file in "$@"; do
+    if [[ ${file} == "http*" ]]; then
+        resolved_link=$(curl -L --head -w '%{url_effective}' ${file} 2>/dev/null | tail -n 1)
+        video_len=$(ffmpeg -i ${resolved_link} 2>&1 | grep "Duration" | cut -d " " -f 4 | sed s/,//)
+        # ffprobe -i $file -show_entries format=duration -sexagesimal -v quiet -of csv="p=0"
+    else
+        video_len=$(ffmpeg -i ${file} 2>&1 | grep "Duration" | cut -d " " -f 4 | sed s/,//)
+    fi
+
     echo ${video_len}
 done
 
