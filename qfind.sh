13c3e720ed55371593935cfc0405385f89b44687
Mehrshad Khansarian <51127725+mehrshad-kh@users.noreply.github.com>
2023-10-09 13:35:09 +0330

    refactor: qfind.sh

--- qfind.sh
+++ qfind.sh
@@ -9,13 +9,13 @@ faculty="https://phee.aut.ac.ir/content/3766/Faculty?&slct_pg_id=3146&sid=17&slc
 homepages=($(curl -L -s "${faculty}" | grep -E "<span.+Homepage" | grep -oE "href=\"[^\"]+\"" | cut -d "=" -f 2 | sed "s/\"//g" | grep -E "^https:\/\/aut\.ac\.ir"))
 
 for homepage in "${homepages[@]}"; do
-    courses=("$(curl -L -s "${homepage}" | sed -n '/Taught Courses/,$p' | grep -E "Quantum" | sed "s/^[^>]*//" | sed "s/>//" | sed "s/>//" | sed "s/<.*//")")
+    courses=("$(curl -L -s "${homepage}" | sed -n '/Taught Courses/,$ p' | grep -E "Quantum" | sed "s/^[^>]*//" | sed "s/>//" | sed "s/>//" | sed "s/<.*//")")
 
     courses=($courses)
     # courses=("$(echo "${courses[@]}" | sort)")
 
     if [[ "${#courses[@]}" -ne 0 ]]; then
-        professor_name="$(echo "${homepage}" | rev | cut -d "/" -f 1 | rev | sed "s/%20/ /g" | sed "s/-/ /g" | sed "s/?slc_lang//g")"
+        professor_name="$(echo "${homepage}" | rev | cut -d "/" -f 1 | rev | sed "s/(%20|-)/ /g" | sed "s/?slc_lang//g")"
         echo "${professor_name}"
         for course in "${courses[@]}"; do
             echo "${course}"
