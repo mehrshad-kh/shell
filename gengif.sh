daf082cdf4f054052c8a936e44b9ff6f476d8041
mehrshad-kh <mjii@protonmail.com>
2023-12-08 22:12:18 +0330

    fix (gengif): improve option handling

--- gengif.sh
+++ gengif.sh
@@ -2,13 +2,24 @@
 # Generate a GIF
 # From: https://superuser.com/a/556031/1756780
 
-if [[ $# -ne 3 ]]; then
-    >&2 echo 'usage: gengif.sh [seconds] [input] [output]'
+zmodload zsh/zutil
+zparseopts t:=duration || exit 1
+
+if [[ $# -ne 4 ]]; then
+    >&2 echo 'usage: gengif.sh -t [duration] [input] [output]'
     exit 1
 fi
 
-ffmpeg -t $1 -i $2 \
+input_file=$3
+output_file=$4
+
+if [[ -f ${output_file} ]]; then
+    >&2 echo "error: ${output_file} already exists"
+    exit 1
+fi
+
+ffmpeg -t ${duration[2]} -i ${input_file} \
     -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
-    -loop 0 $3 
+    -loop 0 ${output_file} 
 
 exit 0
