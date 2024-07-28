d6ae8dd62c321845a6aca2bee34cc5c71ca5b0b1
mehrshad-kh <mjii@protonmail.com>
2024-07-28 23:09:28 +0330

    feat: add chrono.sh

---
+++ chrono.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+function chronograph()
+{
+  start=$(date +%s)
+  while true;
+  do
+    time_elapsed=$(expr $(date +%s) - $start)
+    printf "%s\r" $(date -u -r $time_elapsed +%H:%M:%S)
+    sleep 0.5
+  done
+}
+
+chronograph
